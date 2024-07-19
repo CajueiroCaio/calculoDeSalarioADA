f25ae4ad466ec9dd3cdbbe1714cb6f0a63d3fa4c
CajueiroCaio <caiovcajueiro@gmail.com>
2024-07-19 20:05:38 -0300

    Commit Prova

--- src/Main.java
+++ src/Main.java
@@ -43,11 +43,11 @@ public class Main {
     public static double calculoDescontoINSS(double salario) {
         if (salario <= 1212.00) {
             return salario * 0.075;
-        } else if (salario >= 1212.01 && salario <= 2427.35) {
+        } else if (salario <= 2427.35) {
             return salario * 0.09;
-        } else if (salario >= 2427.36 && salario <= 3641.03) {
+        } else if (salario <= 3641.03) {
             return salario * 0.12;
-        } else if (salario >= 3641.04 && salario <= 7087.22) {
+        } else if (salario <= 7087.22) {
             return salario * 0.14;
         } else {
             return 7087.22 * 0.14;
@@ -57,11 +57,11 @@ public class Main {
     public static double calculoDescontoIR(double salario) {
         if (salario <= 1903.98) {
             return 0.0;
-        } else if (salario >= 1903.99 && salario <= 2826.65) {
+        } else if (salario <= 2826.65) {
             return salario * 0.075;
-        } else if (salario >= 2826.66 && salario <= 3751.05) {
+        } else if (salario <= 3751.05) {
             return salario * 0.15;
-        } else if (salario >= 3751.06 && salario <= 4664.68) {
+        } else if (salario <= 4664.68) {
             return salario * 0.225;
         } else {
             return salario * 0.275;
